f7152f9f9c37d99c3c02f00d6cd658fec69f7ec1
lhnguyen <lehoangnguyen510@gmail.com>
2025-01-16 22:27:28 +0100

    save

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ import json
 
 from data_preprocess.utils import *
 
-if __name__ == "main":
-    with open(osp.join("dataset", "yes_no_dataset.json"), "r") as f:
+if __name__ == "__main__":
+    with open(osp.join("dataset", "generated_yes_no", "train_dataset.json"), "r") as f:
+        dataset = json.load(f)
+    print(f"Dataset length: {len(dataset)}")
+
+    with open(osp.join("dataset", "generated", "val_dataset.json"), "r") as f:
         dataset = json.load(f)
     print(f"Dataset length: {len(dataset)}")
 
